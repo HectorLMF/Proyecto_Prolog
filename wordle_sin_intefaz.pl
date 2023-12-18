414a33590cdb4c6985d6b7782c101ae15c769913
Maronho <hector.ghallegho@gmail.com>
2023-12-18 13:29:06 +0000

    Modificado wordle sin intefaz grafica, refinamos logica de diccionario!

--- wordle_sin_intefaz.pl
+++ wordle_sin_intefaz.pl
@@ -9,7 +9,8 @@ jugar :-
 jugarConDiccionario(UsuarioPalabra, Diccionario) :-
     length(Diccionario, LongitudDiccionario),
     LongitudDiccionario > 0, % Verificar que haya palabras disponibles en el diccionario
-    repetirJuego(UsuarioPalabra, Diccionario).
+    repetirJuego(UsuarioPalabra, Diccionario),
+    writeln('Diccionario actualizado: '), writeln(Diccionario). % Imprimir diccionario actualizado tras cada turno
 
 % Si el diccionario está vacío, imprimir mensaje de finalización
 jugarConDiccionario(_, _) :-
@@ -26,27 +27,56 @@ compararPalabras(PalabraUsuario, PalabraAleatoria, ArrayComun) :-
 generarArrayComun(PalabraUsuario, PalabraAleatoria, ArrayComun) :-
     string_chars(PalabraUsuario, CharsUsuario),
     string_chars(PalabraAleatoria, CharsAleatoria),
-    intersection(CharsUsuario, CharsAleatoria, ArrayComun),
-    writeln('Array de caracteres comunes: '), writeln(ArrayComun).
+    intersection(CharsUsuario, CharsAleatoria, ArrayComun).
+
 
 % Elegir una palabra aleatoria de la misma longitud y con caracteres comunes
 elegirNuevaPalabra(PalabraUsuario, Diccionario, ArrayComun, NuevaPalabra) :-
     writeln('Elegir nueva palabra...'),
     writeln('Palabra del usuario: '), writeln(PalabraUsuario),
     writeln('Diccionario: '), writeln(Diccionario),
-    writeln('Array de caracteres comunes: '), writeln(ArrayComun),
+    % Verificar que PalabraUsuario esté instanciada
+    nonvar(PalabraUsuario),
     atom_length(PalabraUsuario, Longitud),
     palabraDeLongitudConComunes(Longitud, Diccionario, ArrayComun, NuevaPalabra),
+    writeln('---------------nuevo turno------------------'),
     writeln('Palabra aleatoria elegida: '), writeln(NuevaPalabra),
+    purgarDiccionario(NuevaPalabra, PalabraUsuario, Diccionario, DiccionarioFiltrado),
+    write('Palabras candidatas: '),
+    writeln(DiccionarioFiltrado),
     comprobarVictoria(PalabraUsuario, NuevaPalabra).
+    
 
-% Repetir el juego hasta que la palabra elegida por Prolog sea igual a la introducida por el usuario
+
+
+purgarDiccionario(NuevaPalabra, PalabraUsuario, Diccionario, DiccionarioFiltrado) :-
+    string_chars(PalabraUsuario, CharsUsuario),
+    string_chars(NuevaPalabra, CharsAleatoria),
+    intersection(CharsUsuario, CharsAleatoria, ArrayComun),
+    list_to_set(ArrayComun, ArrayComunSinRepetir),
+    
+    writeln('Vamos a purgar, como Stalin...'),
+    write(NuevaPalabra), write(" =? "), writeln(PalabraUsuario),
+    write('Array de caracteres comunes sin repeticiones: '), writeln(ArrayComunSinRepetir),
+
+    % Filtrar el diccionario eliminando las palabras que no contienen al menos un carácter común
+    include(tieneCaracterComun(ArrayComunSinRepetir), Diccionario, DiccionarioFiltrado),
+    write('Palabras candidatas para el proximo turno: '), writeln(DiccionarioFiltrado).
+
+% Predicado auxiliar para verificar si una palabra tiene al menos un carácter común
+tieneCaracterComun(ArrayComun, Palabra) :-
+    member(C, ArrayComun),
+    sub_atom(Palabra, _, 1, _, C).
+
+
+
 % Repetir el juego hasta que la palabra elegida por Prolog sea igual a la introducida por el usuario
 repetirJuego(PalabraUsuario, Diccionario) :-
     writeln('Repetir juego'),
     elegirNuevaPalabra(PalabraUsuario, Diccionario, _, NuevaPalabra),
     writeln('Palabra aleatoria: '), writeln(NuevaPalabra),
     compararPalabras(PalabraUsuario, NuevaPalabra, _),
+    writeln('Diccionario actualizado: '), writeln(Diccionario), % Imprimir diccionario actualizado tras cada turno
     (PalabraUsuario \= NuevaPalabra ->
         writeln('Sigue intentándolo...'),
         jugarConDiccionario(PalabraUsuario, Diccionario)
@@ -68,7 +98,13 @@ palabraDeLongitudConComunes(Longitud, Diccionario, ArrayComun, Palabra) :-
     intersection(Chars, ArrayComun, _). % Verificar que tenga caracteres comunes
 
 % Diccionario de palabras (puedes extenderlo según sea necesario)
-dictionary(['casa', 'perro', 'gato', 'mesa', 'silla', 'coche', 'playa', 'libro', 'agua', 'sol']).
+dictionary([
+    'casa', 'perro', 'gato', 'mesa', 'silla', 'coche', 'playa', 'libro', 'agua', 'sol',
+    'oso', 'robot', 'nube', 'cielo', 'luz', 'radio', 'hoja', 'puente', 'nido', 'techo',
+    'roca', 'fuego', 'llama', 'abeja', 'viento', 'flor', 'tren', 'nieve', 'ciudad', 'planta',
+    'tigre', 'mariposa', 'río', 'globo', 'relámpago', 'niebla', 'fruta', 'tierra', 'campo', 'arcoiris',
+    'diente', 'sombrero', 'escalera', 'pájaro', 'delfín', 'manzana', 'teléfono', 'tormenta', 'reloj', 'camino'
+]).
 
 % Iniciar el juego
 :- jugar.
